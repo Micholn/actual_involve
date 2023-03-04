a653f7ce94fbc3b836a994be80b0e5a50456e12f
Micholn <mdiagbare@gmail.com>
2023-03-04 02:06:45 +0100

    made the patch

--- server/index.js
+++ server/index.js
@@ -3,9 +3,14 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose'
 import cors from 'cors';
 
+
 import postRoutes from './routes/posts.js'
 
 const app = express();
+
+import dotenv from "dotenv";
+dotenv.config();
+
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -15,7 +20,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 
-const CONNECTION_URL = 'mongodb+srv://Enver:west1234@cluster0.csnj5.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = process.env.PORT || 5000;
 
 
