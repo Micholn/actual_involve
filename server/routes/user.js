5c24507679bc2f6ec46c6dc63681dd4e6e4fa0d4
Micholn <mdiagbare@gmail.com>
2023-03-05 13:04:02 +0100

    added the user routes and schema for the Database

---
+++ server/routes/user.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const router = express.Router();
+
+import { signin, signup } from "../controllers/user.js";
+
+router.post("/sigin", signin);
+router.post("/signup", signup);
+
+export default router;
