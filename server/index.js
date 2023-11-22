3d4d04305ae340c486174acf8d646bc5cbae4da3
Micholn <mdiagbare@gmail.com>
2023-11-22 20:50:09 +0100

    filled in the index files for the backend

--- server/index.js
+++ server/index.js
@@ -6,9 +6,14 @@ import cors from "cors";
 const app = express();
 
 
+
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+
+const CONNECTION_URL = process.env.CONNECTION_URL
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL)
      .then(() => app.listen(PORT, ()=> console.log(`Server is running on: ${PORT}`)))
