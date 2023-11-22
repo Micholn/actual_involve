05b0ee719b0bc71db03853097ea1854dee9632be
Micholn <mdiagbare@gmail.com>
2023-11-22 20:45:29 +0100

    filled in the index files for the backend

--- server/index.js
+++ server/index.js
@@ -10,5 +10,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-
+mongoose.connect(CONNECTION_URL)
+     .then(() => app.listen(PORT, ()=> console.log(`Server is running on: ${PORT}`)))
+     .catch((error) => console.log(`${error} did not connect`))
 
