d55493414ba2da073597c157a2abc5fef02b92a4
Micholn <mdiagbare@gmail.com>
2023-11-22 20:40:18 +0100

    filled in the index files for the backend

--- server/index.js
+++ server/index.js
@@ -7,4 +7,8 @@ const app = express();
 
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use()
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+
+
