26b3cf86997a94a66bd294d2067a112ed6755770
Micholn <mdiagbare@gmail.com>
2023-11-22 21:10:13 +0100

    started on the routes creation

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+
+import postRoutes from "/routes/posts.js";
+import userRoutes from "./routes/user.js";
+
 const app = express();
 
 import dotenv from "dotenv";
