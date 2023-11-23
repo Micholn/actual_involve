f70a6b7de33d772e528a4b05d40377a521ad7607
Micholn <mdiagbare@gmail.com>
2023-11-23 09:26:49 +0100

    created the patches for the backend

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,6 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
 
-import PostMessage 
+import PostMessage from "../models/postMessage.js";
+
+const router = express.Router();
+
+export const getPosts = async (req, res) => {
+    try {
+      const PostMessages = await 
+    } catch(error) {
+        res.status(404).json({ message: error.message });
+    }
+} 
 
 
