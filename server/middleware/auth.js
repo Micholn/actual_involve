f63ccfcd44cfdba972df45ead9bff20c5016cd8f
Micholn <mdiagbare@gmail.com>
2023-11-23 08:56:21 +0100

    created the patches for the backend

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,8 +3,19 @@ import jwt from "jsonwebtoken";
 const auth = (req, res, next) => {
     try {
      const token = req.headers.authorization.split(" ")[1];
-     const isCustomAuth = 
+     const isCustomAuth = token.length < 500;
 
+     let decodedData;
+
+     if (token && isCustomAuth){
+        decodedData = jwt.verify(token, secret);
+
+        req.userId = decodedData?._id;
+     } else {
+        decodedData = jwt.decode(token);
+
+        req.userId = decodedData?.sub;
+     }
 
      next();   
     } catch {
