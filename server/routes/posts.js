58b4bb379b756229ff840bc9fd26f7b0dde4596d
Micholn <mdiagbare@gmail.com>
2023-11-23 08:34:15 +0100

    added the post patch and puts

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get("/", getPosts);
 router.post("/", auth, createPost);
 router.patch("/:id", auth, updatePost);
-
+router.delete()
 
 
 export default router;
