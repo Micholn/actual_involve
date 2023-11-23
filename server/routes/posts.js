ad027d4120e256ba78fd2e11ec3809f41bff4e75
Micholn <mdiagbare@gmail.com>
2023-11-23 08:38:24 +0100

    created the patches for the backend

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -7,6 +7,7 @@ router.get("/", getPosts);
 router.post("/", auth, createPost);
 router.patch("/:id", auth, updatePost);
 router.delete("/:id", auth, deletePost);
+router.patch
 
 
 export default router;
