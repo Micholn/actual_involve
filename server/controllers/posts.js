83452c101cbcd8d0198b0553e975205ae463c5cb
Micholn <mdiagbare@gmail.com>
2023-11-23 10:03:48 +0100

    created the patches for the backend

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -49,5 +49,9 @@ export const updatePost = async(req, res) => {
 
     if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
-    const updatedPost = { creator, title, message, tags, selectedFile, }
+    const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+
+    await postMessage.findByIdAndUpdate(id, updatePost, { new: true });
+
+    
 }
