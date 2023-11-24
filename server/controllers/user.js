76094ebe5aba544b942ef11a43b2dd13961f577a
Micholn <mdiagbare@gmail.com>
2023-11-24 23:06:26 +0100

    created the login for the server

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -13,7 +13,7 @@ export const signin = async(req, res) => {
 
 
       } catch (err) {
-          res.status(500).json
+          res.status(500).json({ message: "Something went wrong"});
       }
 
       
