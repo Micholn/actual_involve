497d80f4159d0cad9efbc822d15bb39a878caadc
Micholn <mdiagbare@gmail.com>
2023-11-24 23:34:20 +0100

    created the sigin for the server

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -20,17 +20,11 @@ export const signin = async(req, res) => {
         const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, {expiresIn: "1h"});
 
         res.status(200).json({ result: oldUser, token });
-
       } 
       
       catch (err) {
           res.status(500).json({ message: "Something went wrong"});
       }
-
-      
-
-
-
 }
 
 
