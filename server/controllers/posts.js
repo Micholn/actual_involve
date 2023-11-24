82ba226b0be2396777687a015b0d077b9a61cef9
Micholn <mdiagbare@gmail.com>
2023-11-24 22:12:33 +0100

    created the login for the server

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -53,5 +53,41 @@ export const updatePost = async(req, res) => {
 
     await postMessage.findByIdAndUpdate(id, updatePost, { new: true });
 
-    
+    res.json(updatedPost);
 }
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+
+    if(!mongoose.Types.objectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json({ message: "Post deleted successfully."});
+}
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+
+    if(!req.userId) {
+        return res.json({ message: "Unauthenticated"});
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    const post = await PostMessage.findById(id);
+
+    const index = post.likes.findIndex((id) => id === String(req.userId));
+
+    if(index === -1) {
+        post.likes.push(req.userId);
+    } else {
+        post.likes = post.likes.filter((id) => id !== String(req.userId));
+    }
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true});
+
+    res.status(200).json(updatedPost);
+}
+
+export default router;
---
+++ server/controllers/user.js
@@ -0,0 +1,13 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+import UserModal from "../models/user.js";
+
+const secret = "test";
+
+export const signin = async(req, res) => {
+
+}
+
+
+export const signup
