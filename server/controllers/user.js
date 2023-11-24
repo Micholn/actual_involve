2cf747ef762035a5c5a1acd8fc98da391557d27e
Micholn <mdiagbare@gmail.com>
2023-11-24 23:21:47 +0100

    created the sigin for the server

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -15,7 +15,9 @@ export const signin = async(req, res) => {
 
         const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
 
-        if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid "})
+        if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials"});
+
+        
 
 
 
