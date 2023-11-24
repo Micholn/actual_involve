918183a02c7f6ad28ae4cb28465e24039de45553
Micholn <mdiagbare@gmail.com>
2023-11-24 23:29:50 +0100

    created the sigin for the server

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -17,7 +17,7 @@ export const signin = async(req, res) => {
 
         if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials"});
 
-        const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret)
+        const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, {expiresIn: })
 
 
 
