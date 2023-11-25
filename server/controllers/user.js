f4aae201c936fe083fed9b8dd75b098f18a26786
Micholn <mdiagbare@gmail.com>
2023-11-25 08:25:11 +0100

    created the sigup for the server

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -31,9 +31,10 @@ export const signin = async(req, res) => {
 export const signup = async (req, res) => {
    const { email, password, firstName, lastName } = req.body;
       try {
-
+         const oldUser = await 
       }
       catch(err) {
-         res.status(500).json({ })
+         res.status(500).json({ message: "Something went wrong"});
+         console.log(error);
       }
 }
