23017f2090661cc5268ae5c779d68b966cf41cba
Micholn <mdiagbare@gmail.com>
2023-11-25 08:35:45 +0100

    created the sigup for the server

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -39,7 +39,7 @@ export const signup = async (req, res) => {
 
          const result = await UserModal.create({ email, password: hashedPassword, name: `${firstName} ${lastName}`});
 
-         const token = jwt.sign()
+         const token = jwt.sign({ email: result.email})
       }
       catch(err) {
          res.status(500).json({ message: "Something went wrong"});
