fb96ba4f570926c5ba3cb3dc753818c593963a07
Micholn <mdiagbare@gmail.com>
2023-11-25 08:28:27 +0100

    created the sigup for the server

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -33,7 +33,9 @@ export const signup = async (req, res) => {
       try {
          const oldUser = await UserModal.findOne({ email });
          
-         if (oldUser) return res.status(400).json({ message: "User already exists"})
+         if (oldUser) return res.status(400).json({ message: "User already exists"});
+
+         const hashedPassword = await bcrypt.hash(password, 12);
       }
       catch(err) {
          res.status(500).json({ message: "Something went wrong"});
