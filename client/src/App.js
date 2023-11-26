432e6e31aaac5ec180d7e060c2e98c09bd5331ae
Micholn <mdiagbare@gmail.com>
2023-11-26 12:57:29 +0100

    reordered the root directory of the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Container, Grow, Grid } from "@material-ui/core";
 import { useDispatch } from "react-redux";
+//import Navbar from "./components/Navbar/Navbar"
 
 import './App.css';
 
