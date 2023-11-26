f757c981cb5358cd071b7d59e5a1b45a33a3b8e2
Micholn <mdiagbare@gmail.com>
2023-11-26 13:19:49 +0100

    reordered the root directory of the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -17,13 +17,13 @@ const App = () => {
 
   useEffect(() => {
     dispatch(getPosts());
-  }, [])
+  }, [currentId, dispatch]);
 
 
   return (
     <Container>
        <Navbar/>
-       <Grow>
+       <Grow in>
            <Container>
               <Grid container justify="space-between" alignItems="stretch" spacing={3} >
                  <Grid item xs={12} sm={7}>
