98ae8c03b57d8e836f2ff71426b1d00fa695c756
Micholn <mdiagbare@gmail.com>
2023-11-27 23:24:01 +0100

    setting the roots for the ui

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import thunk from "redux-thunk";
 import { reducers } from "./reducers";
 import './index.css';
 import App from './App';
-import reportWebVitals from 
+import reportWebVitals 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
