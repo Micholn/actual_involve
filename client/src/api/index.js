b2561787f05b915b56359dd4a03456cb1106252b
Micholn <mdiagbare@gmail.com>
2023-11-28 23:26:13 +0100

    added the api endpoint to the frontend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -8,5 +8,5 @@ API.interceptors.request.use((req) => {
         req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("profile")).token}`;
     }
 
-    return req
+    return req;
 });
