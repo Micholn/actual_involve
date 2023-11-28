96ca1206f3eb0f07f59c75bcc808257b666f5176
Micholn <mdiagbare@gmail.com>
2023-11-28 23:23:29 +0100

    added the api endpoint to the frontend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,6 +5,6 @@ const API = axios.create({ baseURL: "http://localhost:5000" });
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem("profile")) {
-        req.headers.Authorization = `Bearer ${JSON.parse()}`
+        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem)}`
     }
 });
