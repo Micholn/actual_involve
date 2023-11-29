ccf99f74c0c0ad20b903a274d639e5f9903ad529
Micholn <mdiagbare@gmail.com>
2023-11-29 09:39:24 +0100

    added the api endpoint to the frontend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -15,4 +15,4 @@ export const fetchPosts = () => API.get("./posts");
 export const createPost = (newPost) => API.post("/posts", newPost);
 export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
-export const 
+export const deletePost
