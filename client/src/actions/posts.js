32e13406dab2fa675c0387c173a650f794595c50
Micholn <mdiagbare@gmail.com>
2023-11-29 22:17:19 +0100

    added the api endpoint to the frontend

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -2,6 +2,8 @@ import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from "../constants/actionType
 import * as api from "../api/index.js";
 
 export const getPosts = () => async (dispatch) => {
-    
+    try {
+        const {data }
+    }
 }
 
