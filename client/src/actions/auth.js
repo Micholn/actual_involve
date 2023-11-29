7fba1a8fac40bc6a38e117bdcd0e01ed9a75aeac
Micholn <mdiagbare@gmail.com>
2023-11-29 10:07:40 +0100

    added the api endpoint to the frontend

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,9 +6,18 @@ export const signin = (formData, router) => async (dispatch) => {
     try {
         const { data } = await api.signIn(formData);
 
-        dispatch({ type: AUTH, data})
+        dispatch({ type: AUTH, data});
+
+        router.push("/");
     } catch (error) {
         console.log(error);
     }
 };
 
+export const signup = (formData, router) => async(dispatch) => {
+    try {
+        const 
+    } catch (error) {
+        console.log(error)
+    }
+}
