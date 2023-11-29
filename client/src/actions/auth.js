aa2089ad24f9091050307674b6f459c0ec6a89ad
Micholn <mdiagbare@gmail.com>
2023-11-29 10:01:43 +0100

    added the api endpoint to the frontend

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,2 +1,12 @@
 import { AUTH } from "../constants/actionTypes";
 import * as api from "../api/index.js";
+
+
+export const signin = (formData, router) => async (dispatch) => {
+    try {
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
