8c7533b6c39797907eaca01b3701a3a3e5dc547e
Micholn <mdiagbare@gmail.com>
2023-11-30 16:33:25 +0100

    worked on the create post section of the frontend

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -13,7 +13,9 @@ export const getPosts = () => async (dispatch) => {
 
 export const createPost = (post) => async (dispatch) => {
     try {
-        const { data } = await  api.createPost
+        const { data } = await  api.createPost(post);
+
+        dispatch({ })
     }
 }
 
