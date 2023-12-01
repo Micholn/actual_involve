ef20d59f1efc02cc54f2241e7744fbde674db051
Micholn <mdiagbare@gmail.com>
2023-12-01 20:56:44 +0100

    added the updatepost section of the frontend

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -19,7 +19,7 @@ export const createPost = (post) => async (dispatch) => {
     } catch (error) {
         console.log(error);
     }
-}
+};
 
 export const updatePost = (id, post) => async (dispatch) => {
     try { 
@@ -33,7 +33,7 @@ export const updatePost = (id, post) => async (dispatch) => {
 } 
 
 export const likePost = (id)  => async (dispatch) => {
-    const { user } = JSON.parse(localStorage.getItem(profile));
+    const  user  = JSON.parse(localStorage.getItem(profile));
 
     try {
         const { data } = await api.likePost(id, user?.token);
