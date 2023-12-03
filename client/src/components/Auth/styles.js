a8704e6f5f1bf852d5c0e98e869513b6f89bc585
Micholn <mdiagbare@gmail.com>
2023-12-03 17:59:21 +0100

    added the auth components of the frontend

--- client/src/components/Auth/styles.js
+++ client/src/components/Auth/styles.js
@@ -24,5 +24,7 @@ export default makeStyles((theme) => ({
     submit: {
         margin: theme.spacing(3, 0, 2),
     },
-    googleButton:  
+    googleButton: {
+        marginBottom: 
+    } 
 }))
