40d37cfe573334c8ac3207632c6b45b513cc31ec
Micholn <mdiagbare@gmail.com>
2023-12-03 18:16:21 +0100

    added the auth components of the frontend

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -6,13 +6,16 @@ import involve from "../../images/involve.png";
 
 
 const Navbar = () => {
+    
     const classes = useStyles();
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
            <Typography className={classes.heading} variant="h2" align="center">
               Involve
            </Typography>
-           <img className={classes.image} src={involve} height="60" />
+           <img className={classes.image} src={involve} height="60" alt="icon" />
         </AppBar>
      )
 }
+
+export default Navbar;
