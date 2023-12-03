57c18b5d21c990434554d40e68ef0d76ea85599c
Micholn <mdiagbare@gmail.com>
2023-12-03 18:20:56 +0100

    added the auth components of the frontend

--- client/src/components/Navbar/styles.js
+++ client/src/components/Navbar/styles.js
@@ -2,6 +2,9 @@ import { makeStyles } from "@material-ui/core";
 
 export default makeStyles(() => ({
     appBar: {
-        borderRadius
+        borderRadius: 15,
+        margin: "30px 0",
+        display: "flex",
+        flexDirection
     }
 }))
