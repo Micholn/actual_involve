3975d38ffddc07684da586a005918764ac47bad8
Micholn <mdiagbare@gmail.com>
2023-12-03 17:48:22 +0100

    added the auth components of the frontend

--- client/src/components/Auth/styles.js
+++ client/src/components/Auth/styles.js
@@ -1,5 +1,8 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
-    
+    paper: {
+        marginTop: theme.spacing(8),
+        display
+    }
 }))
