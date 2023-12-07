2f7386a1bcce7c04bd50782fa66adb396dcdf50d
Micholn <mdiagbare@gmail.com>
2023-12-08 00:08:58 +0100

    added the form for the frontend

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -10,7 +10,7 @@ const Form = ({   }) => {
 
   return (
     <Paper className={classes.paper}>
-      <form autoComplete="off" noValidate >
+      <form autoComplete="off" noValidate className={``}>
 
       </form>
     </Paper>      
