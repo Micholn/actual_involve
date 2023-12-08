4096be675c7456f5cbe18d1ae50494d1e93146e8
Micholn <mdiagbare@gmail.com>
2023-12-08 07:58:17 +0100

    added the callback for tags

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -12,10 +12,10 @@ const Form = ({   }) => {
     <Paper className={classes.paper}>
       <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
         <Typography variant="h6"></Typography>
-        <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e)=> setPostData({ ...postData, creator: e.target.value})} />
+        <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e)=> setPostData({ ...postData, creator: e.target.value })} />
         <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
-        <TextField name="message" variant="outlined" label="message" fullWidth value={postData.message} onchange={(e)=> setPostData({ ...postData, message: })} />
-        <TextField name="tags" variant="outlined" label="Tags (coma separated)" fullWidth value={} />
+        <TextField name="message" variant="outlined" label="message" fullWidth value={postData.message} onchange={(e)=> setPostData({ ...postData, message: e.target.value })} />
+        <TextField name="tags" variant="outlined" label="Tags (coma separated)" fullWidth value={postData.tags} />
         <div></div>
         <Button></Button>
         <Button></Button>
