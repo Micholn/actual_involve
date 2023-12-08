e262d09ba0bbc868840ba3d310fb175fdab932b1
Micholn <mdiagbare@gmail.com>
2023-12-08 07:53:21 +0100

    added the form for the frontend

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -12,8 +12,8 @@ const Form = ({   }) => {
     <Paper className={classes.paper}>
       <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
         <Typography variant="h6"></Typography>
-        <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e)=> setPostData({...postData, creator: e.target.value})} />
-        <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({})} />
+        <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e)=> setPostData({ ...postData, creator: e.target.value})} />
+        <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value})} />
         <TextField name="message" variant="outlined" label="message" fullWidth value={} />
         <TextField name="tags" variant="outlined" label="Tags (coma separated)" fullWidth value={} />
         <div></div>
