da5a204e48a3a780cabbff0220a6593ea4b73937
Micholn <mdiagbare@gmail.com>
2023-12-09 08:52:48 +0100

    added the hooks for the form

--- client/src/components/Form/styles.js
+++ client/src/components/Form/styles.js
@@ -1,5 +1,21 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
-    root: 
-}))
+    root: {
+        '': {
+
+        }
+    },
+    paper: {
+
+    }, 
+    form: {
+
+    },
+    fileInput: {
+
+    },
+    buttonSubmit: {
+
+    },
+}));
