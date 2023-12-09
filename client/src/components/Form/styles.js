ca074e591acd4d4edb2d33458ef757dcf84378c1
Micholn <mdiagbare@gmail.com>
2023-12-09 08:57:31 +0100

    added the hooks for the form

--- client/src/components/Form/styles.js
+++ client/src/components/Form/styles.js
@@ -7,15 +7,18 @@ export default makeStyles((theme) => ({
         }
     },
     paper: {
-
+      padding: theme.spacing(2),
     }, 
     form: {
-
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'center',
     },
     fileInput: {
-
+      width: '97%',
+      margin: '10px 0',
     },
     buttonSubmit: {
-
+      marginBottom: 10,
     },
 }));
