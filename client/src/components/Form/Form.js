8fb24e94b709b20e59ed518dd62785d223128713
Micholn <mdiagbare@gmail.com>
2023-12-09 04:22:29 +0100

    added the hooks for the form

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -7,7 +7,7 @@ import useStyles from "./styles";
 import { createPost, updatePost } from "../..//actions/posts";
 
 const Form = ({ currentId, setCurrentId }) => {
-  const [postData, setPostData] = useState({ })
+  const [postData, setPostData] = useState({ creator: "",  })
 
   return (
     <Paper className={classes.paper}>
