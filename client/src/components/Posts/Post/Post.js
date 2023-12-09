2aefeaaf9f00d8a55ddde266c85571c4aff5d3f2
Micholn <mdiagbare@gmail.com>
2023-12-09 12:58:34 +0100

    added the styling for the authenticated posts

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -26,9 +26,9 @@ const Post = ({ post, setCurrentId }) => {
         <div className={classes.details}>
             <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag}`)}</Typography>
         </div>
-        <Typography className={classes.title}>{post.title}</Typography>
+        <Typography className={classes.title} gutterBottom variant="h5" component="h2">{post.title}</Typography>
         <CardContent>
-            <Typography></Typography>
+            <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
         </CardContent>
         <CardActions className={classes.cardActions}>
            <Button></Button>
