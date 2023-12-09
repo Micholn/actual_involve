ca2ca00b19e47c82942f2c561d87c56bd6e00c09
Micholn <mdiagbare@gmail.com>
2023-12-09 09:10:23 +0100

    added the posts from the authenticated user

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,3 +1,6 @@
 import React from "react";
 import { Card, CardActions, CardContent, CardMedia, Button, Typography } from "@material-ui/core";
 import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MoreHorizon from "@material-ui/icons/MoreHoriz";
+import moment from "moment";
