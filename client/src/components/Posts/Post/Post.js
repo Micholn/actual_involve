f0b343727d24e635a56521ee7614f03805230e2e
Micholn <mdiagbare@gmail.com>
2023-12-09 09:44:32 +0100

    added the cardContents

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -17,8 +17,8 @@ const Post = ({ post, setCurrentId }) => {
      <Card className={classes.card}>
         <CardMedia className={classes.media} image={post.selectedFile || } />
         <div className={classes.overlay}>
-          <Typography>{post.creator}</Typography>
-          <Typography>{}</Typography>
+          <Typography variant="h6">{post.creator}</Typography>
+          <Typography >{moment(post.createdAt).fromNow()}</Typography>
         </div>
         <div className={classes.overlay2}>
 
