ba4f518e77a7c9f1129c8a84ebbad770bb0642d3
Micholn <mdiagbare@gmail.com>
2023-12-09 12:51:26 +0100

    added the styling for the authenticated posts

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -21,10 +21,10 @@ const Post = ({ post, setCurrentId }) => {
           <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
         </div>
         <div className={classes.overlay2}>
-          <Button style={{ color: 'white' }} size="small" onClick={()=> setCurrentId(post._id)}></Button>
+          <Button style={{ color: 'white' }} size="small" onClick={()=> setCurrentId(post._id)}><MoreHorizIcon fontSize="default" /></Button>
         </div>
         <div className={classes.details}>
-
+            
         </div>
         <Typography className={classes.title}>{}</Typography>
         <CardContent>
