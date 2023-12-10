ab5d634af31ac67fc34272257f53b3384d9d128d
Micholn <mdiagbare@gmail.com>
2023-12-10 23:59:24 +0100

    added the ensure LoggedIn on the route of the web app

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ router.get('/', function(req, res, next) {
     escapeXML,redirecct.push(chat)
 })
 
-router.get("calender", ensureLoggedIn)
+router.get("calender", ensureLoggedInn)
 
 
 import express from 'express';
