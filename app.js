dd81f0c7344259bcf884084a736012f50c0b50b6
Micholn <mdiagbare@gmail.com>
2023-12-11 01:49:50 +0100

    added web server files on the app.js

--- app.js
+++ app.js
@@ -25,5 +25,7 @@ var app = express();
 var server = http.createServer(ap);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.set(express.static(path.join(__dirname, 'public')))
+app.set(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
