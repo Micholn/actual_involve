e5e539c12ab3636e521d34a7283682c6e4fd79ca
Micholn <mdiagbare@gmail.com>
2023-12-11 02:12:00 +0100

    added the user login-sessions on the app.js

--- app.js
+++ app.js
@@ -40,5 +40,6 @@ app.use(passport.authenticate('session'));
 app.use(function (req, res, next){
     var msgs = req.session.messages || [];
     res.locals.messages = msgs;
-    
+    res.locals.hasMessages
+
 })
