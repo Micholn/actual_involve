dc35db372d2bdb1d8100e67db68796e4e124685c
Micholn <mdiagbare@gmail.com>
2023-12-11 01:23:03 +0100

    added the route for the account on the index.js

--- app.js
+++ app.js
@@ -3,7 +3,11 @@
 //Server port 
 const PORT = 80;
 
+//Imports 
 var express = require('express');
 var http = require('http');
 var createError = require('http-errors');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var session = require('express-session');
+var sessionStore = require('connect-sqlite3')(session);
--- index.js
+++ index.js
@@ -53,37 +53,5 @@ router.get("/room", ensureLoggedIn, load, function(req, res, next) {
     res.render('room', { user: req.user });
 })
 
-
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose'
-import cors from 'cors';
-
-
-import postRoutes from './routes/posts.js'
-import userRouter from './routes/user.js'
-
-const app = express();
-
-import dotenv from "dotenv";
-dotenv.config();
-
-
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
-
-
-app.use('/posts', postRoutes);
-app.use('/user', userRouter)
-
-
-const CONNECTION_URL = process.env.CONNECTION_URL
-const PORT = process.env.PORT || 5000;
-
-
-
-mongoose.connect(CONNECTION_URL)
-    .then(() => app.listen(PORT, () => console.log(`server is running on port: http://localhost:${PORT}`)))
-    .catch((error) => console.log(`${error} did not connect`));
-
+//Export 
+module.exports = router;
