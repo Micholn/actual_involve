afdc6fe25f509103d4612776ea854ccb9cba44c1
Micholn <mdiagbare@gmail.com>
2023-12-12 15:23:34 +0100

    added the necessary modules for authentication

--- auth.js
+++ auth.js
@@ -5,4 +5,7 @@ var express = require('express');
 var passport = require('passport');
 var password = require('passport-local');;
 var crypto = require('crypto');
-var email = require('@sendgrid/mail')
+var email = require('@sendgrid/mail');
+
+//Database
+var db
