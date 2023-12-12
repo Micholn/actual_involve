7d4704082205eda8041cf5b8f1f7e3afbb24ed8e
Micholn <mdiagbare@gmail.com>
2023-12-12 15:07:08 +0100

    added the initial middleware

--- app.js
+++ app.js
@@ -63,11 +63,11 @@ initSocketIO(server, datsbase, ai);
 //errors
 app.use(function (req, res, next) {
     next(createError(404));
-})
+});
 app.use(function(err, req, res, next) {
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
     res.status(err.status || 500 );
     res.render('error');
-})
+});
 
