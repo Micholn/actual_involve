a946eb597b7c42e13555f6f4094a3be70e22c108
Micholn <mdiagbare@gmail.com>
2023-12-13 23:54:22 +0100

    did the authentication

--- auth.js
+++ auth.js
@@ -30,7 +30,9 @@ passport.use(new password(function verify(username, password, cb) {
     //Check password
     var hashedPassword = db.hashedPassword(password, row.salt);
     if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
-        return cb(null, false, { message: 'Incorrect email or password'})
-    }
+        return cb(null, false, { message: 'Incorrect email or password.'});
+    }  
+
+    //
   });
 }));
