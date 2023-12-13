ebbda12ca7f7eb83ac8b02d3d02592a631e56d25
Micholn <mdiagbare@gmail.com>
2023-12-13 23:20:13 +0100

    did the authentication

--- auth.js
+++ auth.js
@@ -20,4 +20,7 @@ var DOMAIN = "drinks.chat";
 email.setApiKey("SCJNJNJNJNJNJNJNRYDJDKKDD");
 
 // verify user password via database 
-passport.use(new password(function verify(req, res, next)))
+passport.use(new password(function verify(username, password, cb)) {
+  //Query User
+  username
+})
