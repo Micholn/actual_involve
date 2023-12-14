2d7f51b3842d780b38ac21b37b43daa3b5d5f264
Micholn <mdiagbare@gmail.com>
2023-12-14 21:59:16 +0100

    wrote the initial logic for the signup part of the authentication

--- auth.js
+++ auth.js
@@ -60,4 +60,4 @@ passport.deserializeUser(function(user, cb) {
 router.get("/login", function(req, res, next) { res.render('login');  });
 router.get("/siginup", function(req, res, next) { res.render('signup'); });
 router.get('/password', function(req, res, next) { res.render('password'); });
-
+router.get("/reset", function(req, res, next) { res.render('reset'); });
