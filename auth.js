a6a2ac084543c1beceec09c51c091a375ce61f4a
Micholn <mdiagbare@gmail.com>
2023-12-15 23:58:15 +0100

    created the logic for the password reset

--- auth.js
+++ auth.js
@@ -80,6 +80,7 @@ router.post("/password", function(req, res, next) {
            //show
            return res.render('password', { hasMessages: true, messages: ['check your email: ' + username]});
 
+
            
         })
     
