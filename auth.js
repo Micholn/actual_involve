d3ef6e07aba521143d512c9352f8d869d38871df
Micholn <mdiagbare@gmail.com>
2023-12-16 22:28:04 +0100

    created the logic for the change password specifics

--- auth.js
+++ auth.js
@@ -122,5 +122,5 @@ router.post('/signup', function(req, res, next) {
   var salt = crypto.randomBytes(16);
   var hashedPassword = db.hashPassword(req.body.password, salt );
   username = req.body.username.toLowerCase();
-  db.run('INSERT INTO users (username, email, name )')
+  db.run('INSERT INTO users (username, email, name, hashed_password, salt) VALUES (?, ?, ?, ?, ?)', [])
 })
