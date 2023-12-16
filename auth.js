837727bb7c531d3e11a0608cfaffafd67dc9876b
Micholn <mdiagbare@gmail.com>
2023-12-16 22:15:34 +0100

    created the logic for the change password specifics

--- auth.js
+++ auth.js
@@ -120,4 +120,6 @@ router.post('/reset', function(req, res, next) {
 // Sign up 
 router.post('/signup', function(req, res, next) {
   var salt = crypto.randomBytes(16);
+  var hashedPassword = db.hashPassword(req.body.password, salt );
+  username = req.body.username
 })
