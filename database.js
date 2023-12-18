82c25dbf98df3a840d988a5e37ead63073d38455
Micholn <mdiagbare@gmail.com>
2023-12-18 23:51:44 +0100

    wrote the logic for creating the tables

--- database.js
+++ database.js
@@ -36,7 +36,7 @@ db.serialize(function() {
     db.run('UPDATE users SET username=? WHERE');
 
     // Clean users 
-    db.run
+    db.run("")
 
     // Create times table 
     db.run("CREATE TIMES TABLE IF NOT EXISTS times () ")
