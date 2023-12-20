cd3b24d26e9b0771b0a4c2628afd2f591028a7fa
Micholn <mdiagbare@gmail.com>
2023-12-20 16:50:22 +0100

    added the rooms section to the database

--- database.js
+++ database.js
@@ -84,6 +84,6 @@ db.serialize(function() {
 db.storeMessage = function(room, username, message) {
     const query = 'INSERT INTO chat_history (room, username, message) VALUES (?, ?, ?)';
     db.all(query, [room, username, message], (err, result) => {
-        
+        if (err)
     })
 }
