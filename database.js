49add1529cabda679782ac2a12e53894943ac205
Micholn <mdiagbare@gmail.com>
2023-12-20 17:01:44 +0100

    added the logic for loading chat history to the database

--- database.js
+++ database.js
@@ -91,5 +91,5 @@ db.storeMessage = function(room, username, message) {
 
 db.loadChatHistory = function(room, callback) {
     const query = 'SELECT * FROM chat_history WHERE room = ? ORDER BY timestamp';
-    db.all(query, [room], (err, ))
+    db.all(query, [room], (err, results))
 }
