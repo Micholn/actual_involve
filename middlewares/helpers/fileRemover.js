aca7008e767e89b7b227eaf4575f7b718b909580
Micholn <mdiagbare@gmail.com>
2023-12-21 23:22:39 +0100

    added files for the middleware

--- middlewares/helpers/fileRemover.js
+++ middlewares/helpers/fileRemover.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 module.exports = files => {
     return Promise.all(
         files.map(
-            new Promise()
+            new Promise((res, rej) =>)
         )
     )
 }
