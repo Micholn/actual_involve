10f674af3a6e69fa8d3a5bc68c089937190e7bc1
Micholn <mdiagbare@gmail.com>
2023-12-21 23:28:33 +0100

    added files for the middleware

--- middlewares/helpers/fileRemover.js
+++ middlewares/helpers/fileRemover.js
@@ -5,6 +5,7 @@ module.exports = files => {
         files.map(
             new Promise((res, rej) => {
                 try {
+                    setTimeout
 
                 } catch (err) {
                     console.error(err);
