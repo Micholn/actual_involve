2e3f18c98bfa7898f11f899792bd265076195d17
Micholn <mdiagbare@gmail.com>
2023-12-21 23:43:11 +0100

    added files for the middleware

--- middlewares/helpers/multer.js
+++ middlewares/helpers/multer.js
@@ -5,5 +5,6 @@ const multer = require("multer");
 const storageByUser = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, file.fieldname)
-    }
+    },
+    filename: 
 })
