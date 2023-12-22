1982396b1952888d2ed96d05c4d6e93ad9f32c15
Micholn <mdiagbare@gmail.com>
2023-12-22 01:20:50 +0100

    added the strorageUser

--- middlewares/helpers/multer.js
+++ middlewares/helpers/multer.js
@@ -16,6 +16,7 @@ const storageByUser = multer.diskStorage({
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './public/uploads ')
-    }
+        cb(null, './public/uploads')
+    },  
+    filename: 
  })
