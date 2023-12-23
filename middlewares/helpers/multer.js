9749f779b3e48e1a7ac743406fd946da6fa42915
Micholn <mdiagbare@gmail.com>
2023-12-23 22:11:19 +0100

    added the multer storage

--- middlewares/helpers/multer.js
+++ middlewares/helpers/multer.js
@@ -29,6 +29,6 @@ const storage = multer.diskStorage({
         cb(null, './public/uploads')
     },
     filename: function (req, file, cb) {
-        cb(null, file.filename + '-' + req.admin.role )
+        cb(null, file.filename + '-' + req.admin.role +  )
     }
  })
