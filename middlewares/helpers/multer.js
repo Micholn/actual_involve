1831e9701cf58f9ff146d8c7d13e5218ada31f3f
Micholn <mdiagbare@gmail.com>
2023-12-24 00:54:17 +0100

    created the super admin's storage

--- middlewares/helpers/multer.js
+++ middlewares/helpers/multer.js
@@ -35,7 +35,7 @@ const storage = multer.diskStorage({
 
  const fileFilter = (res, file, callback) => {
     const ext = path.extname(fileoriginalname);
-    if (ext !== '.png' && ext !== '.jpg' && ext !== '.JPG') {
+    if (ext !== '.png' && ext !== '.jpg' && ext !== '.JPG' && ) {
 
     }
  }
