41f8af66614c21a9b4409f8bfe6fa5617f60934d
Micholn <mdiagbare@gmail.com>
2023-12-25 10:15:26 +0100

    added the uploading logic of the admin

--- middlewares/helpers/multer.js
+++ middlewares/helpers/multer.js
@@ -52,4 +52,4 @@ exports.uploadAdminDoc = multer({ storage, fileFilter, limits }).fields([
 
 exports.uploadAdminDoc = multer({ storage, fileFilter, limits }).single("doc");
 exports.uploadAdminPhoto = multer({ storage, fileFilter, limits }).single("photo");
-exports.uploadUserPhoto = 
+exports.uploadUserPhoto = multer({ storage: })
