d250797d14e22d0c5c58280628b47f79b74901c6
Micholn <mdiagbare@gmail.com>
2023-12-28 10:01:21 +0100

    added the mailing automation

--- middlewares/helpers/mailer.js
+++ middlewares/helpers/mailer.js
@@ -4,7 +4,8 @@ exports.sendEmail = mailngData => {
    const transporter = nodeMailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
-    secure: 
+    secure: false,
+    
    })
 
 
