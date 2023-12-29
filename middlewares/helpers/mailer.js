b9895e11b1582f9760c2165aa6e87b33f1a795ac
Micholn <mdiagbare@gmail.com>
2023-12-29 01:36:16 +0100

    added the mail logic

--- middlewares/helpers/mailer.js
+++ middlewares/helpers/mailer.js
@@ -17,7 +17,7 @@ exports.sendEmail = mailngData => {
         console.log(`Message sent: ${info.response}`)
       })
       .catch(err => {
-        console.log(`Problem sending email: `)
+        console.log(`Problem sending email: ${}`)
       })
 
 
