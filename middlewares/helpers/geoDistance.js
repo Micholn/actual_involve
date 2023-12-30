26dfc42777c6805ef2261610f537f43af9e32df2
Micholn <mdiagbare@gmail.com>
2023-12-30 23:28:30 +0100

    added the geo distance

--- middlewares/helpers/geoDistance.js
+++ middlewares/helpers/geoDistance.js
@@ -4,5 +4,5 @@ module.exports = (lon1, lat1, lon2, lat2 ) => {
     // distance between latitude and longitude  in radians
     const dLat = ((lat2 - lat1) * Math.PI) /180;
     const dLon = ((lon2 - lon1) * Math.PI) / 180;
-    
+    // haversine
 }
