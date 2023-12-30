92f7a5a5d4129ea47733f9c5e9035eba8b82866a
Micholn <mdiagbare@gmail.com>
2023-12-30 23:25:20 +0100

    added the geo distance

--- middlewares/helpers/geoDistance.js
+++ middlewares/helpers/geoDistance.js
@@ -2,5 +2,5 @@ module.exports = (lon1, lat1, lon2, lat2 ) => {
     // mean radius of earth's = 6,371km
     const R = 6371;
     // distance between latitude and longitude  in radians
-    const dLat = ((lat2 - lat1))
+    const dLat = ((lat2 - lat1) * Math.PI) /180
 }
