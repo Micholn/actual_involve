803d5036c55ae9fec489c4f3bbd66d1551e53ed4
Micholn <mdiagbare@gmail.com>
2023-12-31 01:29:45 +0100

    exported the get distance func

--- middlewares/helpers/createNotification.js
+++ middlewares/helpers/createNotification.js
@@ -17,6 +17,8 @@ module.exports = async(io, adminId, notificationObj) => {
         let notifications = notificationObjOfAdmin.notofications
         notifications.unshift(notificationObj)
         notificationObjOfAdmin.noOfUnseen += 1 
-        if (notificationObjOfAdmin.noOfUnseen < 20 && notifications.length > 20)
+        if (notificationObjOfAdmin.noOfUnseen < 20 && notifications.length > 20) {
+            
+        }
     }
 }
