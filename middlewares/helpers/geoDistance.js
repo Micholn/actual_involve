6b61249ed8297361927d6beb22d1b5dc202c8353
Micholn <mdiagbare@gmail.com>
2023-12-31 01:07:13 +0100

    exported the get distance func

--- middlewares/helpers/geoDistance.js
+++ middlewares/helpers/geoDistance.js
@@ -12,5 +12,5 @@ module.exports = (lon1, lat1, lon2, lat2 ) => {
         Math.sin(dLon / 2) * 
         Math.sin(dLon / 2);
     const c =  2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const d = R * 
+    const d = R * c;
 }
