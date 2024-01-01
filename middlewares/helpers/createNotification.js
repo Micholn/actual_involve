ab281c3d47c4383e622191f374ffefef01974df3
Micholn <mdiagbare@gmail.com>
2024-01-01 21:41:04 +0100

    reworked the notifications

--- middlewares/helpers/createNotification.js
+++ middlewares/helpers/createNotification.js
@@ -27,7 +27,7 @@ module.exports = async(io, adminId, notificationObj) => {
     if (socketUser.length) {
         //for every same login user emit notification
         socketUser.forEach(u => {
-            io.to(u.socketId).emit
+            io.to(u.socketId).emit()
         })
     }
 }
