c9b69c0f30d4751da5ff1f01033951754c6e3dd7
Micholn <mdiagbare@gmail.com>
2024-01-02 17:10:11 +0100

    added the requirements for the no relational database

--- middlewares/helpers/dbConnection.js
+++ middlewares/helpers/dbConnection.js
@@ -13,7 +13,7 @@ module.exports = () => {
         .then(() => console.log("DB connected"))
         .catch(err => {
             console.error(
-                "Failed to connect to the database"
+                "Failed to connect to the database on startup"
             )
         })
 }
