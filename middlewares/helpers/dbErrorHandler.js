0e891bc4d9d0698dfbc44da1a3af0b833cb66111
Micholn <mdiagbare@gmail.com>
2024-01-03 21:41:53 +0100

    added the error handler

--- middlewares/helpers/dbErrorHandler.js
+++ middlewares/helpers/dbErrorHandler.js
@@ -10,7 +10,7 @@ return s
 
 exports.errorHandler=(e => { let s="", if(e.code) switch(e.code) { 
     case 11001:s=uniqueMessage(e) } 
-    else{-1!==e.message.indexOf }
+    else{-1!==e.message.indexOf("Cast to ObjectId") }
 
 })
 
