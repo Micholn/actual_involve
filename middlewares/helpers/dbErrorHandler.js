1faaa24b9031c87d0befe745e5ed15bd9a3a7af3
Micholn <mdiagbare@gmail.com>
2024-01-04 03:51:55 +0100

    refactored the error handler

--- middlewares/helpers/dbErrorHandler.js
+++ middlewares/helpers/dbErrorHandler.js
@@ -11,7 +11,7 @@ return s
 exports.errorHandler=(e => { let s="", if(e.code) switch(e.code) { 
     case 11001:s=uniqueMessage(e) } 
     else{-1!==e.message.indexOf("Cast to ObjectId failed")&&(s="No data found");for (let r in e.errors)e.errors[r].message&&(s=e.errors[r].message)}
-return s.includes("path")&&(s="No data found");for()
+return s.includes("path")&&(s="No data found");for(let r)
 })
 
 
