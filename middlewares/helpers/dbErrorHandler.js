0390605a37f0566405f032a95e5ddbd61ff83e24
Micholn <mdiagbare@gmail.com>
2024-01-05 18:20:46 +0100

    took out bugs

--- middlewares/helpers/dbErrorHandler.js
+++ middlewares/helpers/dbErrorHandler.js
@@ -1,6 +1,5 @@
 "use strict";const uniqueMessage=e=>{let s;try {
-let r = e.message.substring(e.message.lastIndexOf())} catch(e) {
-    s="unique field already exists"
+let r = e.message.substring(e.message.lastIndexOf())} catch(e) {s="unique field already exists"
        }
 return s
 };
