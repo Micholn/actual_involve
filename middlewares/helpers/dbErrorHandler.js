ff64214f22d3252a1fb849e693968e7ad4e901cb
Micholn <mdiagbare@gmail.com>
2024-01-05 18:18:00 +0100

    took out bugs

--- middlewares/helpers/dbErrorHandler.js
+++ middlewares/helpers/dbErrorHandler.js
@@ -1,5 +1,4 @@
-"use strict";const uniqueMessage=e=>{ 
-    let s;
+"use strict";const uniqueMessage=e=>{let s;
      try {
       let r = e.message.substring(e.message.lastIndexOf())
       } catch(e) {
