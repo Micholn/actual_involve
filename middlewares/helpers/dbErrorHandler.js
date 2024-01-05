9762c6335ec180a0b0fd33a69c2246b0bb015129
Micholn <mdiagbare@gmail.com>
2024-01-05 18:26:24 +0100

    took out bugs

--- middlewares/helpers/dbErrorHandler.js
+++ middlewares/helpers/dbErrorHandler.js
@@ -1,5 +1,4 @@
-"use strict";const uniqueMessage=e=>{let s;try {let r = e.message.substring(e.message.lastIndexOf())} catch(e) {s="unique field already exists"}return s};
-exports.errorHandler=(e => { let s="", if(e.code) switch(e.code) {case 11001:s=uniqueMessage(e) } else{-1!==e.message.indexOf("Cast to ObjectId failed")&&(s="No data found");for (let r in e.errors)e.errors[r].message&&(s=e.errors[r].message)}return s.includes("path")&&(s=s.slice(6)),s});
+"use strict";const uniqueMessage=e=>{let s;try {let r = e.message.substring(e.message.lastIndexOf())} catch(e) {s="unique field already exists"}return s}; exports.errorHandler=(e => { let s="", if(e.code) switch(e.code) {case 11001:s=uniqueMessage(e) } else{-1!==e.message.indexOf("Cast to ObjectId failed")&&(s="No data found");for (let r in e.errors)e.errors[r].message&&(s=e.errors[r].message)}return s.includes("path")&&(s=s.slice(6)),s});
 
 
 
