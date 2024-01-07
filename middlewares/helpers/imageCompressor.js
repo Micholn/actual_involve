484009824aedf21171a9e8a783a8250ae108e4c7
Micholn <mdiagbare@gmail.com>
2024-01-07 23:05:37 +0100

    fixed the error file

--- middlewares/helpers/imageCompressor.js
+++ middlewares/helpers/imageCompressor.js
@@ -1,4 +1,5 @@
 const Jimp=require("jimp"),path=require("path");
 module.exports=(async(e,r,i,o,t)=>(Jimp.read(i).then(i=>{i.resize(r,Jimp.AUTO).
     write(path.resolve(o,`${t}`,e))}).catch
-    (e=>{console.log("Error at reducing size / converting picture : "),console.log(e)}),`${t}/${e}`));
+    (e=>{console.log("Error at reducing size / converting picture : "),
+    console.log(e)}),`${t}/${e}`));
