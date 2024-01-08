ad45340593b6503901c291ab58de4dd8e1ecc627
Micholn <mdiagbare@gmail.com>
2024-01-08 12:53:49 +0100

    added the authentication for the dataase

--- middlewares/user_actions/auth.js
+++ middlewares/user_actions/auth.js
@@ -18,6 +18,8 @@ exports.isAuthenticatedUser = asyncErrorHandler(async (req, res, next) => {
 
 exports.authorizeRoles = (...roles) => {
     return (req, res, next) => {
-        if (!roles.includes)
+        if (!roles.includes(req.user.role)) {
+            return 
+        }
     }
 }
