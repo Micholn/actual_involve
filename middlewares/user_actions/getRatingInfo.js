46cf9dfaea0f706450e76892655fc2d6a2af1c80
Micholn <mdiagbare@gmail.com>
2024-01-09 10:39:28 +0100

    added the getRatingInfo

--- middlewares/user_actions/getRatingInfo.js
+++ middlewares/user_actions/getRatingInfo.js
@@ -1,5 +1,7 @@
 const Review = require("../../models/Review")
 module.exports = async(product,newStar) => {
     const product = req.product
-    if (!product.isVerified && )
+    if (!product.isVerified && product.isDeleted) {
+        
+    }
 }
