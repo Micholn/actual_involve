9af653b0a915d50ce89acb5b9b561943fe300348
Micholn <mdiagbare@gmail.com>
2024-01-09 10:57:43 +0100

    added the getRatingInfo

--- middlewares/user_actions/getRatingInfo.js
+++ middlewares/user_actions/getRatingInfo.js
@@ -16,5 +16,5 @@ module.exports = async(product,newStar) => {
     //this condition is executed during postReview and editReview 
     if (newStar === 5) fiveStars += 1
     if (newStar === 4) fourStars += 1
-    if ()
+    if (newStar === 3) fourStars += 1
 }
