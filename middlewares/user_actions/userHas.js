b4e5441eedc867ce730d8a267e3783d56c0f1c84
Micholn <mdiagbare@gmail.com>
2024-01-10 23:43:55 +0100

    did the patch for the getRating

--- middlewares/user_actions/userHas.js
+++ middlewares/user_actions/userHas.js
@@ -3,4 +3,4 @@ const Review = require("../../models/Review")
 const Order = require("../../models/Order")
 const Wjislist = require("../../models/Wishlist")
 
-module.exports = async()
+module.exports = async(product,user)
