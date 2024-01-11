90edc4d5a2e61f291ed6f24aed3a83fd4c9c788a
Micholn <mdiagbare@gmail.com>
2024-01-11 23:01:05 +0100

    did the patch for the user has database

--- middlewares/user_actions/userHas.js
+++ middlewares/user_actions/userHas.js
@@ -18,7 +18,7 @@ module.exports = async(product,user,type) => {
 
         //wishList bahek 
         if (type !=='wishlists') {
-            hasOnWishList = await Whishlist.findOne({ user: })
+            hasOnWishList = await Whishlist.findOne({ user: user._id, })
         }
     }
 }
