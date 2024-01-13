accdf1cd5dfc0ea5f224d39ecc6e171a6db4916e
Micholn <mdiagbare@gmail.com>
2024-01-13 03:10:34 +0100

    added the cart and models to the database

--- middlewares/validator/index.js
+++ middlewares/validator/index.js
@@ -6,5 +6,6 @@ const path = require("path")
 const fs = require("fs");
 const { districts } = require("../common");
 exports.validateLead = (req, res, next) => {
-    //email is not null 
+    //email is not null, valid and normalized
+    req.check
 }
