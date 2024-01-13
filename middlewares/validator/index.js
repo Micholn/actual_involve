0f3a48a777cb31ad4d7b5d802fec7c2d7547a760
Micholn <mdiagbare@gmail.com>
2024-01-13 03:06:49 +0100

    added the cart and models to the database

--- middlewares/validator/index.js
+++ middlewares/validator/index.js
@@ -2,3 +2,4 @@ const ProductBrand = require("../../models/ProductBrand")
 const ProductImages = require("../../models/ProductImages")
 const Category = require("../../models/Category")
 const _ = require('lodash')
+const path 
