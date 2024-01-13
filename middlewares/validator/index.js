38e585dbb83cd3011ccd2b1b21cb19ec30b9f42d
Micholn <mdiagbare@gmail.com>
2024-01-13 03:12:29 +0100

    added the cart and models to the database

--- middlewares/validator/index.js
+++ middlewares/validator/index.js
@@ -7,5 +7,5 @@ const fs = require("fs");
 const { districts } = require("../common");
 exports.validateLead = (req, res, next) => {
     //email is not null, valid and normalized
-    req.check("email", "Email")
+    req.check("email", "Email must be between 3 to 32 characters")
 }
