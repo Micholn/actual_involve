03964d4ca9b6acdb52ea11000790adde7b1be652
Micholn <mdiagbare@gmail.com>
2024-01-14 10:23:45 +0100

    added the index for the validator

--- middlewares/validator/index.js
+++ middlewares/validator/index.js
@@ -41,5 +41,7 @@ exports.validateSignUp = (req, res, next) => {
         .isLength({ min: 6 })
         .withMessage("Password must contain at least 6 characters")
         .matches(/\d/)
-        .withMessage("Password must contain a number")
+        .withMessage("Password must contain a number");
+    // check for errors
+    
 }
