2ed4f678352418122e754b3321ef029d904a3292
Micholn <mdiagbare@gmail.com>
2024-01-14 10:04:35 +0100

    added the index for the validator

--- middlewares/validator/index.js
+++ middlewares/validator/index.js
@@ -26,4 +26,5 @@ exports.validateLead = (req, res, next) => {
 };
 exports.validateSignUp = (req, res, next) => {
     //name is not null and between 4-10 characters
+    req.check("name", "Name is required").notEmpty();
 }
