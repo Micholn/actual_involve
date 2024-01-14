6d841d71b34a8ee6723a325d07b483c8c665c634
Micholn <mdiagbare@gmail.com>
2024-01-14 10:09:10 +0100

    added the index for the validator

--- middlewares/validator/index.js
+++ middlewares/validator/index.js
@@ -29,4 +29,5 @@ exports.validateSignUp = (req, res, next) => {
     req.check("name", "Name is required").notEmpty();
     // email is not null, valid and normalized 
     req.check("email", "Email must be between 3 to 32 characters")
+        .matches
 }
