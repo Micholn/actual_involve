32eba984da6fb6230f5a9c9d62a1def2e44773a9
Micholn <mdiagbare@gmail.com>
2024-01-15 13:01:08 +0100

    added a fix on the index validator

--- middlewares/validator/index.js
+++ middlewares/validator/index.js
@@ -54,6 +54,7 @@ exports.validateSignUp = (req, res, next) => {
 }
 exports.validateSocialLogin = (req, res, next) => {
     //name is not null and between 4-10 characters  
-    req.check("name", "Name is required").notEmpty()
+    req.check("name", "Name is required").notEmpty();
+    
     req.check()
 }
