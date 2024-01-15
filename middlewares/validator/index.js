dabcba596a95c9974fe1a04b2100037cbcf8537c
Micholn <mdiagbare@gmail.com>
2024-01-15 13:21:33 +0100

    added a fix on the index validator

--- middlewares/validator/index.js
+++ middlewares/validator/index.js
@@ -63,5 +63,5 @@ exports.validateSocialLogin = (req, res, next) => {
             min: 4,
             max: 2000
         });
-    req.check("userID", "userID is required.").notEmpty
+    req.check("userID", "userID is required.").notEmpty()
 }
