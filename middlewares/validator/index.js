7a8f984d39a4a1a73accb79070fce47da15d8217
Micholn <mdiagbare@gmail.com>
2024-01-15 12:42:46 +0100

    added a fix on the index validator

--- middlewares/validator/index.js
+++ middlewares/validator/index.js
@@ -47,6 +47,7 @@ exports.validateSignUp = (req, res, next) => {
     // if errors show the first one as they happen 
     if (errors) {
         const firstError = errors.map(error => error.msg)[0];
-        return res.status(400).json({ error: firstError})
+        return res.status(400).json({ error: firstError });
     }
+    //
 }
