0fa018a48ff081e37d23dd816c3355c10b601c64
Micholn <mdiagbare@gmail.com>
2024-01-16 20:56:25 +0100

    added the dispatcher

--- middlewares/validator/index.js
+++ middlewares/validator/index.js
@@ -65,5 +65,5 @@ exports.validateSocialLogin = (req, res, next) => {
         });
     req.check("userID", "userID is required.").notEmpty()
     req.check("socialPhoto", "Invalid photo url.")
-    .notEmpty
+    .notEmpty()
 }
