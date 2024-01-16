79b1ee342b4805d8e4364722a2fe80e066263950
Micholn <mdiagbare@gmail.com>
2024-01-16 22:11:42 +0100

    added the dispatcher

--- middlewares/validator/index.js
+++ middlewares/validator/index.js
@@ -68,4 +68,5 @@ exports.validateSocialLogin = (req, res, next) => {
     .notEmpty()
     .matches(/[(http(s)?):\/\(www\.)?a-zA-Z0-9@:%._\+~#]{2,6}\b([-a-zA-z0-9@:%_\+.~#?&//=]*)/)
     req.check("loginDomain", "Invalid login domain")
+        .notEmpty
 }
