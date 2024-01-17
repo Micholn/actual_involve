1e0231a1c326f518b44f5abbbddd3e3a07b1b3f7
Micholn <mdiagbare@gmail.com>
2024-01-18 00:59:22 +0100

    added the requirements for the phone numb and email on the middleware

--- middlewares/validator/index.js
+++ middlewares/validator/index.js
@@ -109,6 +109,7 @@ exports.validateDispatcher = (req, res, next) => {
     // if error show the first one as they happen 
     if (errors) {
         const firstError = errors.map(error => error.msg)[0];
-        return res.status(400).json({ error: })
+        return res.status(400).json({ error: firstError });
     }
+    
 }        
