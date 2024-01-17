f82a617c5950020905e7b6eb996479f13bd30023
Micholn <mdiagbare@gmail.com>
2024-01-17 17:12:25 +0100

    added the validate social login to the validator middleware

--- middlewares/validator/index.js
+++ middlewares/validator/index.js
@@ -75,6 +75,7 @@ exports.validateSocialLogin = (req, res, next) => {
     // if error show the first one as they happen 
     if (errors) {
         const firstError = errors.map(error => error.msg)[0];
-        return res.status(400).json({ error: firstError})
+        return res.status(400).json({ error: firstError});
     }
+    //
 }
