c92ab2992295aad9e0d5136bb443c699589730b9
Micholn <mdiagbare@gmail.com>
2024-01-18 00:43:26 +0100

    added the requirements for the phone numb and email on the middleware

--- middlewares/validator/index.js
+++ middlewares/validator/index.js
@@ -98,5 +98,6 @@ const validatedispatcher = req => {
 exports.validateDispatcher = (req, res, next) => {
     validatedispatcher(req)
     // check for password 
-    req.check("password", "Password is required").notEmpty();
+    req.check("password", "Password is required").notEmpty();   
+    req.check
 }
