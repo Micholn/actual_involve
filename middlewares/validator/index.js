32e2250aef5eb669fb2f973e9af32e07675fb9cc
Micholn <mdiagbare@gmail.com>
2024-01-17 17:02:14 +0100

    added the validate social login to the validator middleware

--- middlewares/validator/index.js
+++ middlewares/validator/index.js
@@ -71,5 +71,5 @@ exports.validateSocialLogin = (req, res, next) => {
         .notEmpty()
         isIn(['google', 'facebook'])
     // check for errors
-    const errors = req.validationError()
+    const errors = req.validationError();
 }
