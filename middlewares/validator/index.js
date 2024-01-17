2298a89b42eaed79c56d2c1e3cd4d97f0d883bc1
Micholn <mdiagbare@gmail.com>
2024-01-17 17:24:39 +0100

    added the validate social login to the validator middleware

--- middlewares/validator/index.js
+++ middlewares/validator/index.js
@@ -85,5 +85,6 @@ const validatedispatcher = req => {
     req.check("name", "Name is required").notEmpty();
     // email is not null, valid and normalized 
     req.check("email", "Email must be between 3 to 32 characters")
-        .matches
+        .matches(/.+\@.+\..+/)
+        .withMessage 
 }
