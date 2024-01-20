182051bf3fee13126e54328e2b705c3e7e9501b8
Micholn <mdiagbare@gmail.com>
2024-01-20 07:15:46 +0100

    fixed some bugs

--- middlewares/validator/index.js
+++ middlewares/validator/index.js
@@ -146,8 +146,9 @@ exports.passwordResetValidator = (req, res, next) => {
     //check for errors
     const errors = req.validationErrors();
     // if error show the first one as they happen 
-    if(errors) {
+    if (errors) {
         const firstError = errors.map(error => error.msg)[0];
-        return res.status(400).json({ error: firstError})
+        return res.status(400).json({ error: firstError });
     }
+    // proceed to the nextfunction
 }
