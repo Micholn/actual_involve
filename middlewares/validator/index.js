9efb65e17ae49b26b1511ffebacad952999fd534
Micholn <mdiagbare@gmail.com>
2024-01-21 21:56:37 +0100

    did the business validation on the database

--- middlewares/validator/index.js
+++ middlewares/validator/index.js
@@ -169,7 +169,7 @@ exports.validateBusinessInfo = (req, res, next) => {
         // if (req.files) for (const file in req.files) {
             files.push(req.files[file][0]);
         }
-        files.forEach
+        files.forEach(file => {})
     } 
 
 }
