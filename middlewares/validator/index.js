de5e5c636ff83700afe5d98394dc83e449a7fc9f
Micholn <mdiagbare@gmail.com>
2024-01-21 22:24:48 +0100

    did the business validation on the database

--- middlewares/validator/index.js
+++ middlewares/validator/index.js
@@ -181,4 +181,5 @@ exports.validateBusinessInfo = (req, res, next) => {
 exports.validateAdminBankInfo = (req, res, next) => {
     req.check("accountHolder", "Account holder name is required").notEmpty()
     req.check("bankName", "Bank name is required").notEmpty()
+    req.check()
 }
