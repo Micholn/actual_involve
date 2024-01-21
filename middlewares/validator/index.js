92ff5ef09dfd17f75cb5df044a171892ba5e4750
Micholn <mdiagbare@gmail.com>
2024-01-21 22:14:36 +0100

    did the business validation on the database

--- middlewares/validator/index.js
+++ middlewares/validator/index.js
@@ -175,5 +175,6 @@ exports.validateBusinessInfo = (req, res, next) => {
         const firstError = errors.map(error => error.msg)[0];
         return res.status(400).json({ error: firstErrror });
     } 
+    next();
 
 }
