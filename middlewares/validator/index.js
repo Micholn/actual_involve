b0cc96102ef14f05ab61298b0533dbd0dcf48b13
Micholn <mdiagbare@gmail.com>
2024-01-23 09:06:24 +0100

    validate warehouse steps

--- middlewares/validator/index.js
+++ middlewares/validator/index.js
@@ -202,6 +202,6 @@ exports.validateWareHouse = (req, res, next) => {
     // check for errors
     const errors = req.validationErrors();
     if (errors) {
-        
+        const firstError = 
     }
 } 
