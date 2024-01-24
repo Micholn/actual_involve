920c3127814203541aed388a3c0dfe32985d2040
Micholn <mdiagbare@gmail.com>
2024-01-24 15:34:51 +0100

    reworked the admin profile

--- middlewares/validator/index.js
+++ middlewares/validator/index.js
@@ -231,5 +231,5 @@ exports.validateAdminProfile = (req, res, next) => {
 } 
 exports.validateProduct = async (req, res, next) => {
     req.check("name", "Product name is rquired").notEmpty()
-    req.check("price", "selling price of product is required")
+    req.check("price", "selling price of product is required").notEmpty
 }
