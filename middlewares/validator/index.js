895e005c0c8734583923be00df9c891371a2e3c6
Micholn <mdiagbare@gmail.com>
2024-01-24 14:27:48 +0100

    reworked the admin profile

--- middlewares/validator/index.js
+++ middlewares/validator/index.js
@@ -218,5 +218,5 @@ exports.validateAdminProfile = (req, res, next) => {
         .isLength({ min: 6 })
         .withMessage("must contain a number")
         .matches(/\d/)
-        .withMessage()
+        .withMessage("must contain a number")
 } 
