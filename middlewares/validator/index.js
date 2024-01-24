ed478af4df3682abe02627f09a8d9ab5dade27d3
Micholn <mdiagbare@gmail.com>
2024-01-24 15:27:12 +0100

    reworked the admin profile

--- middlewares/validator/index.js
+++ middlewares/validator/index.js
@@ -224,6 +224,6 @@ exports.validateAdminProfile = (req, res, next) => {
     const errors  = req.validationErrors()
     // if error show show the first one as they happen 
     if (errors) {
-       const firstError = errors.map()
+       const firstError = errors.map(error => error.msg)
     }
 } 
