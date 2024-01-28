b43bfa9bfa1245a5192b5e79ea77f1b76466ff89
Micholn <mdiagbare@gmail.com>
2024-01-28 07:40:22 +0100

    added the image ids

--- middlewares/validator/index.js
+++ middlewares/validator/index.js
@@ -273,6 +273,6 @@ exports.validateProduct = async (req, res, next) => {
     } else if (categories.some(cat=>cat.isDisabled)) {
         errors.push({ msg: "Categories have been disabled"})
     } else { 
-        req.body
+        req.body.category
     }
 }
