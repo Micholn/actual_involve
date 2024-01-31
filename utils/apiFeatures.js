6a7f2a67e7f95021419199e325eaf048bffa4dae
Micholn <mdiagbare@gmail.com>
2024-01-31 11:57:54 +0100

    refactored the search

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -27,7 +27,7 @@ class APIFeatures {
         // Advance filter price, ratings etc
         let queryStr = JSON.stringify(queryCopy);
         queryStr = queryStr.replace(
-            /\b(gt|gte|lt|lte)\b
+            /\b(gt|gte|lt|lte)\b/g,
         )
     }
 }
