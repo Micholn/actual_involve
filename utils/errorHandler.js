eacc07943d500974246764568bb5790526f501fe
Micholn <mdiagbare@gmail.com>
2024-02-02 13:26:19 +0100

    reworked the api features

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -3,6 +3,7 @@ class ErrorHandler extends Error {
         super(message);
         this.statusCode = statusCode 
 
-        Error.captureStackTrace()
+        Error.captureStackTrace(this, this.constructor);
     }
 }
+
