cf8925bbb2431fd8f7dec89fc9345fc88061e584
Micholn <mdiagbare@gmail.com>
2024-02-03 23:12:55 +0100

    added the token

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -11,6 +11,6 @@ const sendToken = (user, statusCode, res) => {
         httpOnly: true,
     };
 
-    res.status(statusCode).cookie("token", token, )
+    res.status(statusCode).cookie("token", token, options).json
 
 }
