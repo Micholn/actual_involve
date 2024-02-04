3954b26d7a9db63adf2866b6c64d0a4a323b1315
Micholn <mdiagbare@gmail.com>
2024-02-05 00:18:36 +0100

    worked on the search feature query

--- utils/searchFeatures.js
+++ utils/searchFeatures.js
@@ -34,6 +34,6 @@ class searchFeatures {
 
         //console.log(JSON.parse(queryString));
 
-        this.query = this.query.find()
+        this.query = this.query.find(JSON.parse(query))
     }
 }
