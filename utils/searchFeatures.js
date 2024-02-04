5677361ca0377b476c1a6ba0ded619669a366faa
Micholn <mdiagbare@gmail.com>
2024-02-05 00:10:46 +0100

    worked on the search feature query

--- utils/searchFeatures.js
+++ utils/searchFeatures.js
@@ -30,6 +30,6 @@ class searchFeatures {
 
         //price filter
         let queryString = JSON.stringify(queryCopy);
-        queryString = queryString.replace()
+        queryString = queryString.replace(/\b(gt|gte|lt|lte)\b/g)
     }
 }
