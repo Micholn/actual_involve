2185675c6f36d032cd76ffbc9bcd3b0ead3d40f1
Micholn <mdiagbare@gmail.com>
2024-02-07 23:05:33 +0100

    added the mail service

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-CONST sendEmail = async (options) => {
+const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         PORT: process.env.SMTP_PORT,
@@ -35,4 +35,4 @@ CONST sendEmail = async (options) => {
     });
 };
 
-
+module.exports = sendMail; 
