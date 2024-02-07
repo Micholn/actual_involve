b7e37a36cb65d5c0f09dd8897fe1d6ef4cba02f9
Micholn <mdiagbare@gmail.com>
2024-02-07 22:30:50 +0100

    added the mail service

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -9,7 +9,7 @@ CONST sendEmail = async (options) => {
         service: process.env.SMTP_SERVICE, 
         auth: {
             user: process.env.SMTP_MAIL,
-            pass: process.env.SMTP_PASSWORD
+            pass: process.env.SMTP_PASSWORD,
         }
     })
 }
