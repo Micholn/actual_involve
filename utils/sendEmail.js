92858ff79d86e368c5ee3968de6d25c6e5cd100e
Micholn <mdiagbare@gmail.com>
2024-02-07 22:51:09 +0100

    added the mail service

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -28,5 +28,5 @@ CONST sendEmail = async (options) => {
         templateId: options.templateId,
         dynamic_template_data: options.data,
     }
-    sgMail.send(msg)
+    sgMail.send(msg).then
 }
