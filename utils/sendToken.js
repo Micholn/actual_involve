56d689f76e1c2f77f99aa5d578bfe13a350f1e9e
Micholn <mdiagbare@gmail.com>
2024-02-08 19:04:32 +0100

    added the token receival email

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -8,5 +8,5 @@ const sendToken = (user, statusCode, res) => {
         httpOnly: true
     }
 
-    res.status(statusCode).cookie('token', token, )
+    res.status(statusCode).cookie('token', token, options)
 }
