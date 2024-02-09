a25d08e354ca58b119dae36e57c56da87ab809eb
Micholn <mdiagbare@gmail.com>
2024-02-09 23:51:50 +0100

    added the ai

--- app/ai.js
+++ app/ai.js
@@ -3,4 +3,6 @@ var ai = {}
 // OpenAI GPT 
 const { Configuration, OpenAI } = require("openai");
 const openai = new OpenAIApi(new Configuration({ apiKey: 'dfggujfgkffgbgg',}));
-ai.sendMessage = function(io, db, room, username, message)
+ai.sendMessage = function(io, db, room, username, message) {
+    //Load 
+}
