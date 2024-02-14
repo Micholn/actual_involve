3fac30b021b69b53fc6483abb6dd276aa946332a
Micholn <mdiagbare@gmail.com>
2024-02-14 09:58:21 +0100

    added the missing part of the schema

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -34,6 +34,10 @@ const Post = ({ post, setCurrentId }) => {
            <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}><ThumbUpAltIcon fontSize="small" />Like {post.likeCount}</Button>
            <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))} ><DeleteIcon fontSize="small" />Delete</Button>
         </CardActions>
+        <CardActions className={classes.cardActions}>
+           <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}><ThumbUpAltIcon fontSize="small" />Like {post.likeCount}</Button>
+           <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))} ><DeleteIcon fontSize="small" />Delete</Button>
+        </CardActions>
      </Card>
    );
 };
