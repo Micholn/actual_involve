7e296ed18b11c6f2ed34472a0441827e7e3df98b
Micholn <mdiagbare@gmail.com>
2024-02-16 00:09:44 +0100

    added the time of creation to the schema

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -11,6 +11,6 @@ export const getPosts = async (req, res) => {
 
         res.status(200).json(postMessages);
     } catch (error) {
-        res.status(404).json()
+        res.status(404).json({})
     }
 }
