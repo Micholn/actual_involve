fb544e37ba8dfabab1e2970498097f76f30aa3b5
Micholn <mdiagbare@gmail.com>
2024-02-18 22:49:45 +0100

    reworked the main server

--- server/index.js
+++ server/index.js
@@ -10,4 +10,4 @@ const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(body)
+app.use(bodyParser)
