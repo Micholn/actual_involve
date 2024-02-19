cee49aca995ffbb9a87f99c2e29523b356353807
Micholn <mdiagbare@gmail.com>
2024-02-19 23:23:07 +0100

    reworked the main server

--- server/index.js
+++ server/index.js
@@ -12,4 +12,4 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.use('/posts', postRoute)
+app.use('/posts', postRoutes)
