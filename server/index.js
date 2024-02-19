619426b1dfccec5468ea6bee0599f4aa6c43982a
Micholn <mdiagbare@gmail.com>
2024-02-20 00:55:36 +0100

    reworked the main server

--- server/index.js
+++ server/index.js
@@ -19,5 +19,5 @@ const PORT = process.env.PORT || 5000;
 
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopolgy: true  })
-    .then(() => app.listen(PORT, () => ))
+    .then(() => app.listen(PORT, () => con))
 
