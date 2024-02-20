7ffd44a08e19945ea63923cea3d352b13ba0ce25
Micholn <mdiagbare@gmail.com>
2024-02-20 07:47:48 +0100

    reworked the main server

--- server/index.js
+++ server/index.js
@@ -19,5 +19,5 @@ const PORT = process.env.PORT || 5000;
 
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopolgy: true  })
-    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://`)))
+    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost`)))
 
