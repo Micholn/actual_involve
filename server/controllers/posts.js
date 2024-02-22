df7aa066c6cd207fc65a84bf4ad0071f789da983
Micholn <mdiagbare@gmail.com>
2024-02-22 23:25:27 +0100

    redid the controllers

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -9,4 +9,4 @@ export const getPosts = async (req, res) => {
     try {
         const postMessages = await PostMessage.find();
 
-        res.status(200).json(postMe
+        res.status(200).json(postM
