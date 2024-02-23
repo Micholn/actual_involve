957a11fd5ccfb00f908a2ac04ad01739d74a4252
Micholn <mdiagbare@gmail.com>
2024-02-23 23:28:11 +0100

    patched the controllers

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -7,6 +7,6 @@ const router = express.Router();
 
 export const getPosts = async (req, res) => {
     try {
-        const postMessages = await PostMessa
+        const postMessages = await PostMess
 
         
