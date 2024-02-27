7e9d70ddf86a8c217f7f95f4515aa12795332349
Micholn <mdiagbare@gmail.com>
2024-02-27 01:40:59 +0100

    refactored the main server file

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -44,4 +44,4 @@ export const signup = async (req, res) => {
          res.status(201).json({ result, token });
       }
       catch (err) {
-         res.status(500).json({ message: "So
+         res.status(500).json({ message: "S
