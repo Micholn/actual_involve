a14b45a674cb52eb852934918ec0b720b621484e
Micholn <mdiagbare@gmail.com>
2024-02-29 03:14:04 +0100

    reworked the user post controllers

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -39,4 +39,4 @@ export const signup = async (req, res) => {
 
          const result = await UserModal.create({ email, password: hashedPassword, name: `${firstName} ${lastName}`});
 
-         const token = jwt.sign({ email: result.email,
+         const token = jwt.sign({ email: result.emai
