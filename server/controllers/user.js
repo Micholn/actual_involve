7f742707e8e0c02fed9398faf3aaaa100e9463c4
Micholn <mdiagbare@gmail.com>
2024-03-01 23:44:23 +0100

    refactored the user modal

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -39,4 +39,4 @@ export const signup = async (req, res) => {
 
          const result = await UserModal.create({ email, password: hashedPassword, name: `${firstName} ${lastName}`});
 
-         const token = jwt.sign({ e
+         const token = jwt.sign({ 
