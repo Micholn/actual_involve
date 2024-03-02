b9ab0d12f5fdab47ab83235baf5a2cf2d49877d9
Micholn <mdiagbare@gmail.com>
2024-03-02 01:04:50 +0100

    refactored the user modal

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -37,4 +37,4 @@ export const signup = async (req, res) => {
 
          const hashedPassword = await bcrypt.hash(password, 12);
 
-         const result = await UserModal.create({ em
+         const result = await UserModal.create({ e
