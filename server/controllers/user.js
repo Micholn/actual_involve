bca24dc5ee0d035e4b58238d25683d966067b02e
Micholn <mdiagbare@gmail.com>
2024-03-04 00:17:35 +0100

    added the signup edit

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -33,4 +33,4 @@ export const signup = async (req, res) => {
       try {
          const oldUser = await UserModal.findOne({ email });
          
-         if (oldUser) return res.
+         if (oldUser) return re
