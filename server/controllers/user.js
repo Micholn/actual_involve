b07ca0806008e570051b6fa0135e5c07608730e1
Micholn <mdiagbare@gmail.com>
2024-03-05 20:49:20 +0100

    reworked the bcrypt token to be extended by timing

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -29,4 +29,4 @@ export const signin = async(req, res) => {
 
 
 export const signup = async (req, res) => {
-   const { email, password, firstName, lastName } = 
+   const { email, password, firstName, lastName } 
