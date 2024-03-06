8f98e47d9095a88e222f40bea421b00736b6b053
Micholn <mdiagbare@gmail.com>
2024-03-06 23:57:19 +0100

    reworked the bcrypt token to be extended by timing

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -23,4 +23,4 @@ export const signin = async(req, res) => {
       } 
       
       catch (err) {
-          res.status(500).json({ message: "Something went wrong"});
+          res.status(500).json({ message: "Something went wrong"}
