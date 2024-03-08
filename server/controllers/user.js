4a0c3634efb540671ca2ea21e2772ee804e1d056
Micholn <mdiagbare@gmail.com>
2024-03-08 22:53:23 +0100

    reworked the status of the user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -17,4 +17,4 @@ export const signin = async(req, res) => {
 
         if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials"});
 
-        const token = jwt.sign({ email: oldUser.em
+        const token = jwt.sign({ email: oldUser.
