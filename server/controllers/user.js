b305fdc8798b33cc80c8fa0489e445df2ca1b8be
Micholn <mdiagbare@gmail.com>
2024-03-09 23:50:39 +0100

    refactored the password authentication

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -16,4 +16,4 @@ export const signin = async(req, res) => {
         const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
 
         if (!isPasswordCorrect) return res.status(400).json({ message: "Inval
-        
+   
