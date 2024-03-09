075e031e8b72dad12fd70b0ed1aecb3e82f83542
Micholn <mdiagbare@gmail.com>
2024-03-10 00:47:52 +0100

    refactored the password authentication

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -13,4 +13,4 @@ export const signin = async(req, res) => {
 
         if(!oldUser) return res.status(404).json({ message: "User doesn't exist"});
 
-        const isPasswordCorrect = await bcrypt.
+        const isPasswordCorrect = await bcrypt
