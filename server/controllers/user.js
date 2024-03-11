aeb1112afd9c51096dbc160be190b36a64577508
Micholn <mdiagbare@gmail.com>
2024-03-11 23:05:52 +0100

    debugged the user failing to connect

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -11,4 +11,4 @@ export const signin = async(req, res) => {
       try {
         const oldUser = await UserModal.findOne({ email });
 
-        if(!oldUser) return res.st
+        if(!oldUser) return re
