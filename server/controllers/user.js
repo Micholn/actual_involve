e23000f0470468fbf49d6d90f033ab9194450ba6
Micholn <mdiagbare@gmail.com>
2024-03-12 13:40:01 +0100

    debugged the user failing to connect

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -9,4 +9,4 @@ export const signin = async(req, res) => {
    const { email, password } = req.body;
 
       try {
-        const oldUser = await UserModal.fin
+        const oldUser = await UserModal.f
