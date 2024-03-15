f73eedcbeae88902465e626adb361ac641d0de23
Micholn <mdiagbare@gmail.com>
2024-03-15 23:42:41 +0100

    refactorization of backend

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -16,7 +16,7 @@ const auth = async (req, res, next) => {
      } else {
         decodedData = jwt.decode(token);
 
-        req.userId = decodedData?.
+        req.userId = decodedData?
 
      next()  
      
