6a4405cd3e8df75c12618c58c81e0cbd4a6b8396
Micholn <mdiagbare@gmail.com>
2024-03-17 20:32:37 +0100

    restarted the controllers for the server

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -7,6 +7,6 @@ const router = express.Router();
 
 export const getPosts = async (req, res) => {
     try {
-        const postMessages = await PostMessage.
+        const postMessages = await PostMessage.find
     }
 }
