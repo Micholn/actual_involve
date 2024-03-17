e5104f4bde2dbc54890b873667a2edd52b9696e0
Micholn <mdiagbare@gmail.com>
2024-03-17 23:30:12 +0100

    took out the user file from the server

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const url = "http://localhost:5000/posts";
 
-
 API.interceptors.request.use((req) => {
     if(localStorage.getItem("profile")) {
         req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("profile")).token}`;
