73b54b18427adce042017bba3a028fc97526ec8b
Micholn <mdiagbare@gmail.com>
2024-03-17 23:27:41 +0100

    took out the user file from the server

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -16,5 +16,3 @@ export const createPost = (newPost) => API.post("/posts", newPost);
 export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
 export const deletePost = (id) => API.delete(`/posts/${id}`);
-
-export const signIn = () => API.post("/user/sigin", formData);
