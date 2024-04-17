933c05a334255c2de1905797b4196f98968494da
Micholn <mdiagbare@gmail.com>
2024-04-17 23:46:09 +0100

    repositioned the library

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ router.get("/account", ensureLoggedIn, load, function(req, res, next){
     res.render('account', { user: req.user });
 })
 
-router.get("/room", ensureLoggedIn, load, function(req, res, next)
+router.get("/room", ensureLoggedIn, load, function(req, res, nex
     res.render('room', { user: req.user });
 })
 
